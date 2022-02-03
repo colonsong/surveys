9f384b8b8fd897ce635eb1170f372fa60d263d29
colin <colin.song@asiayo.com>
2022-02-03 22:27:47 +0800

    register link & add fullname in register

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -2,14 +2,14 @@
 <div>
     <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
     <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
-      Sign in to your account
+      登入
     </h2>
     <p class="mt-2 text-center text-sm text-gray-600">
       Or
       {{ ' ' }}
-      <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
-        start your 14-day free trial
-      </a>
+      <router-link :to="{name: 'Register'}" class="font-medium text-indigo-600 hover:text-indigo-500">
+        註冊
+      </router-link>
     </p>
   </div>
   <form class="mt-8 space-y-6" action="#" method="POST">
--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -7,17 +7,21 @@
   <p class="mt-2 text-center text-sm text-gray-600">
     Or
     {{ ' ' }}
-    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
-      start your 14-day free trial
-    </a>
+    <router-link :to="{name:'Login'}" class="font-medium text-indigo-600 hover:text-indigo-500">
+      登入你的帳號
+    </router-link>
   </p>
 </div>
 <form class="mt-8 space-y-6" action="#" method="POST">
   <input type="hidden" name="remember" value="true" />
   <div class="rounded-md shadow-sm -space-y-px">
+     <div>
+      <label for="fullname" class="sr-only">全名</label>
+      <input id="fullname" name="email" type="email" autocomplete="email" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" />
+    </div>
     <div>
       <label for="email-address" class="sr-only">Email address</label>
-      <input id="email-address" name="email" type="email" autocomplete="email" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" />
+      <input id="email-address" name="name" type="text" autocomplete="name" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" />
     </div>
     <div>
       <label for="password" class="sr-only">Password</label>
