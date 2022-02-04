d550eb199e34c5ca9ddee387ec1ec907d2c18b3e
colin <colin.song@asiayo.com>
2022-02-04 22:50:32 +0800

    vue for register && login api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,18 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rules\Password;
 
 class AuthController extends Controller
 {
     public function register(Request $request) {
         $data = $request->validate([
-                'name' => 'required|stirng',
+                'name' => 'required|string',
                 'email' => 'required|email|string|unique:users,email',
                 'password' => [
                     'required',
                     'confirmed',
-                    Password::min(8)->mixedCase()->numbers()->symbols()
+                    Password::min(8)
                 ]
         ]);
 
@@ -31,6 +32,34 @@ class AuthController extends Controller
             'user' => $user,
             'token' => $token
         ]);
+    }
+
+    public function login(Request $request) {
+        $credentials = $request->validate([
+            'email' => 'required|email|string|unique:users,email',
+            'password' => [
+                'required'
+            ],
+            'remember' => 'boolean'
+        ]);
+
+        $remember = $credentials['remember'] ?? false;
+        unset($credentials['remember']);
+
+        if (!Auth::attempt($credentials, $remember)) {
+            return response([
+                'error' => 'login fail , remember',
+                422
+            ]);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+
     }
 
 }
